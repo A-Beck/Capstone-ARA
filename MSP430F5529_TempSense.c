2ed9927c92ba02acce5a42f1bab5532369c033dc
Andrew Becker <adb2jb@virginia.edu>
2014-11-17 13:36:48 -0500

    Update MSP430F5529_TempSense.c

--- MSP430F5529_TempSense.c
+++ MSP430F5529_TempSense.c
@@ -1,19 +1,21 @@
 #include <msp430.h>
 
+#define INPUT = 0x01
+
 void WDTinit(void);		// sets the watchdog timer (turns it off)
 void ADCinit(void);		// Initializes the ADC to take samples
 void interruptInit(void);	// enables needed interrupts
 void clockInit(void);	// initializes the clocks to correct source and frequency
 
-void sample(void);
-void convert(void);
+void sample(void);  // requests a sample from the ADC
+void convert(void);  // Converts sample to farenhiet
 
 // global variables
 float temp = 0;
 int data = 0;
 
-int main(void)
-{
+int main(void) {
+  // setup
   WDTinit();		// turn off the watchdog timer
   clockInit();	// initialize clocks to run at 1MHZ
   ADCinit();		// set up the ADC for sampling
@@ -24,53 +26,52 @@ int main(void)
   {
     sample();
     convert();
-    _nop();
+    _nop();  // breakpt
     __delay_cycles(10000);
   }
 }
 
+// Convert ADC data to temp in farenheit
 void convert(void) {
 	float millivolts = data * (3300.0/4096.0);  // 3.3 v is reference voltage, 12^2 = 4096 possible bins
 	float celcius = (millivolts - 500.0) / 10.0;  // from data sheet of TMP36G
 	temp = (celcius  * 1.8 + 32.0);  // basic c to f conversion
 }
 
+// Start conversion and read result
 void sample(void) {
-	ADC12CTL0 |= ADC12SC;                   // Start conversion
+	ADC12CTL0 |= ADC12SC;         // Start conversion
 	while (!(ADC12IFG & BIT0));
-	data = ADC12MEM0;		// get data from ADC reading
-	__no_operation();                       // SET BREAKPOINT HERE
-
-
+	data = ADC12MEM0;			  // get data from ADC reading
 }
+
+
+// Enable ADC to sample on demand
+void ADCinit(void){
+	  P6SEL |= INPUT;         // Enable A/D channel A0 - p6.0
+	  REFCTL0 &= ~REFMSTR;    // Reset REFMSTR to hand over control to ADC12_A ref control registers
+	  ADC12CTL0 = ADC12ON + ADC12SHT02 + ADC12REFON;  // turn it on, set sampling time for 16 cycles
+
+	  ADC12CTL1 = ADC12SHP;   // Use sampling timer
+	  ADC12MCTL0 = ADC12SREF_0;   // Vr+=VCC (3.3v) and Vr-=AVss (0V)
+
+	  __delay_cycles(10000);   // let this thing settle
+
+	  ADC12CTL0 |= ADC12ENC;  // Enable conversions
+}
+
+
 
 void WDTinit(void) {
 	WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
-}
-
-
-void ADCinit(void){
-
-	volatile unsigned int i;
-	  P6SEL |= 0x01;                            // Enable A/D channel A0
-	  REFCTL0 &= ~REFMSTR;                      // Reset REFMSTR to hand over control to
-	                                            // ADC12_A ref control registers
-	  ADC12CTL0 = ADC12ON+ADC12SHT02+ADC12REFON;
-	                                            // Turn on ADC12, Sampling time
-	                                            // On Reference Generator and set to
-	                                            // 2.5V
-	  ADC12CTL1 = ADC12SHP;                     // Use sampling timer
-	  ADC12MCTL0 = ADC12SREF_0;                 // Vr+=Vref+ and Vr-=AVss
-
-	  for ( i=0; i<0x30; i++);                  // Delay for reference start-up
-
-	  ADC12CTL0 |= ADC12ENC;                    // Enable conversions
 }
 
 void interruptInit(void) {
 	_BIS_SR(GIE);		// enable global interrupts
 }
 
+
+// set clock to 16MHZ to match IR demo
 void clockInit(void) {
 	UCSCTL0 = 0x0000;                         // Set lowest possible DCOx, MODx
 	UCSCTL1 = DCORSEL_5;                      // Select DCO range 24MHz operation
